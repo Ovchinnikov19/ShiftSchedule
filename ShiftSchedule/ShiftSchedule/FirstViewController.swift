18d2aeb6dec07fe71c2145f763c945f9ef715199
Ovchinnikov19 <r1dd1k1919@gmail.com>
2019-02-07 15:08:53 +0300

    Empty project

---
+++ ShiftSchedule/ShiftSchedule/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ShiftSchedule
+//
+//  Created by Eduard Ovchinnikov on 07/02/2019.
+//  Copyright © 2019 Эдуард Овчинников. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ShiftSchedule/ShiftSchedule/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ShiftSchedule
+//
+//  Created by Eduard Ovchinnikov on 07/02/2019.
+//  Copyright © 2019 Эдуард Овчинников. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
