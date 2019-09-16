9e741746e06d1e5b652a34b22e414fca45ee9b30
Ovchinnikov19 <r1dd1k1919@gmail.com>
2019-09-16 16:41:05 +0300

    Update icons

---
+++ ShiftSchedule/ShiftSchedule/PageCalendarVC.swift
@@ -0,0 +1,30 @@
+//
+//  PageCalendarVC.swift
+//  ShiftSchedule
+//
+//  Created by Eduard Ovchinnikov on 09/04/2019.
+//  Copyright © 2019 Эдуард Овчинников. All rights reserved.
+//
+
+import UIKit
+
+class PageCalendarVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
